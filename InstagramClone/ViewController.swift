56d317876fe405c52681f24813ed9ec7511cdd9d
melikesoygullucu <melikesoygul@hotmail.com>
2024-07-02 21:59:09 +0300

    First commit

--- InstagramClone/ViewController.swift
+++ InstagramClone/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func signInButton(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    @IBAction func SignUpButton(_ sender: Any) {
+    }
 }
 
