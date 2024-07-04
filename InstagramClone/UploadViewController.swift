8bc178c96bf7dfd5f4aab3852828a7eb4c7b08bd
melikesoygullucu <melikesoygul@hotmail.com>
2024-07-04 21:46:52 +0300

    select image with picker added

--- InstagramClone/UploadViewController.swift
+++ InstagramClone/UploadViewController.swift
@@ -7,12 +7,36 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionTextField: UITextField!
+    @IBOutlet weak var shareButtonOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
     }
+    
+    @objc func selectImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func shareButton(_ sender: Any) {
+    }
+    
 
 }
